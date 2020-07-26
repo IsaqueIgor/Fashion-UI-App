18e51857b3f8978382e4978441c364d7f095de04
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-07-25 21:25:16 -0300

    initial commit

---
+++ src/screens/OnBoarding/OnBoarding.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const OnBoarding = () => {
+  return (
+    <div>
+      <h1>Onboarding</h1>
+    </div>
+  );
+};
+
+export default OnBoarding;
